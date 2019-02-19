ce663ca7baf360438a65442a613d86338c6f59a2
Mac HD <machd@Macs-MacBook-Pro.local>
2019-02-19 14:07:21 -0800

    gitnored proxy server

--- proxy/server.js
+++ proxy/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const port = 3030;
 
 
 app.use(express.static(`${__dirname}/../public`));
+
 
 app.listen(port, () => console.log(`serving is running at port ${port}`))
