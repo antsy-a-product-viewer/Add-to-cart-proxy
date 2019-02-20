98d7329119037af7d7d339b2fef321c8e909c1ed
Mac HD <machd@Macs-MacBook-Pro.local>
2019-02-19 17:46:42 -0800

    refactoring code to point to a specific local host, and getting all the pages to render in the same html file

--- proxy/server.js
+++ proxy/server.js
@@ -4,8 +4,7 @@ const app = express();
 
 const port = 3030;
 
-
-app.use(express.static(`${__dirname}/../public`));
+app.use('/product/:productId', express.static(`${__dirname}/./public`))
 
 
 app.listen(port, () => console.log(`serving is running at port ${port}`))
