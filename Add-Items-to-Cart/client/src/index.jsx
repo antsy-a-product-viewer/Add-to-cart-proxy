0aa58b107e170454e529ee9b8639cffbd4b15338
Mac HD <machd@Macs-MacBook-Pro.local>
2019-02-20 20:11:35 -0800

    fixed untracked files

--- Add-Items-to-Cart/client/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Cart from './Components/Cart.jsx';
-
-// window.Cart = Cart;
-
-ReactDOM.render(<Cart />, document.getElementById('store'));
-
-
